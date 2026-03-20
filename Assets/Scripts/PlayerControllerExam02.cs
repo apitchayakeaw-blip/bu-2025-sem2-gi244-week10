55fc43fa9b21ef9c27f3b0042020e9911029cee4
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2026-03-20 16:20:02 +0700

    Exam02

--- Assets/Scripts/PlayerControllerExam02.cs
+++ Assets/Scripts/PlayerControllerExam02.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerExam02 : MonoBehaviour
 {
     public float jumpForce;
     public float gravityModifier;
