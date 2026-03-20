e11cf28f964b8bd4731e7bc6d655c46aefc4b97e
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2026-03-20 15:47:18 +0700

    Exam01

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefab;
     public Vector3 spawnPos = new(25, 0, 0);
 
     public float startDelay = 2;
     public float repeatRate = 2;
 
     private PlayerController playerController;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,11 +18,15 @@ public class SpawnManager : MonoBehaviour
 
         InvokeRepeating(nameof(SpawnObstacle), startDelay, repeatRate);
 
+     
         GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+
+        int obstacle = Random.Range(0, obstaclePrefab.Length);
+        
+        Instantiate(obstaclePrefab[obstacle], spawnPos, obstaclePrefab[obstacle].transform.rotation);
     }
 }
